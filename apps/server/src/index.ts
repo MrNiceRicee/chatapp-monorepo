02f5598d0c5a0718ab386b836b53e635810fbda6
Joshua Santos <JoshCSan@gmail.com>
2023-06-04 21:57:55 -0700

    - chore: slowly adding trpc server

---
+++ apps/server/src/index.ts
@@ -0,0 +1,59 @@
+import fastify from 'fastify';
+import cors from '@fastify/cors';
+import { fastifyTRPCPlugin } from '@trpc/server/adapters/fastify';
+
+import { appRouter, createTRPCContext } from '@rice/api';
+import env from '~/config/env';
+
+const envToLogger = {
+  development: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  },
+  production: true,
+  test: false,
+};
+
+async function server() {
+  const app = fastify({
+    logger: envToLogger[env.NODE_ENV] || true,
+    maxParamLength: 5000,
+  });
+
+  app.get('/health', (_request, reply) => {
+    return reply.send('ok');
+  });
+
+  await app.register(cors, {
+    // options
+  });
+
+  void app.register(fastifyTRPCPlugin, {
+    prefix: '/trpc',
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    trpcOptions: { router: appRouter, createContext: createTRPCContext },
+  });
+
+  return app;
+}
+
+async function main() {
+  const app = await server();
+
+  try {
+    await app.listen({
+      port: env.SERVER_PORT,
+      host: env.SERVER_HOST,
+    });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+}
+
+void main();
