587aed2e385be2eb8df22a9c7f7e5507aea7a888
Joshua Santos <JoshCSan@gmail.com>
2023-06-04 21:22:50 -0700

    - refactor: healthRouter

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,4 +1,5 @@
-import { publicProcedure, createTRPCRouter } from "~/trpc";
+import { createTRPCRouter } from '~/trpc';
+import { healthRouter } from './routers/health/healthRouter';
 
 /**
  * This is the primary router for your server.
@@ -6,10 +7,8 @@ import { publicProcedure, createTRPCRouter } from "~/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  health: publicProcedure.query(() => {
-    return 'ok';
-  })
-})
+  health: healthRouter,
+});
 
 // export type definition of the API
 export type AppRouter = typeof appRouter;
