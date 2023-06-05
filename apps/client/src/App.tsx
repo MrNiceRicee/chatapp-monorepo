19af27ad22120c888d99c289fe521dd4facca9b1
Joshua Santos <47019696+MrNiceRicee@users.noreply.github.com>
2023-06-04 18:47:29 -0700

    feat: Env validation (#1)
    * - chore: removing test stuff
    
    * - feat: env check

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import { hello } from '@rice/api';
 import './App.css';
 
 function App() {
@@ -18,7 +17,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>{hello('josh')}</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
