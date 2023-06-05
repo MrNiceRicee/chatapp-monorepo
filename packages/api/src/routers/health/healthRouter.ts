5ebf354d8562565e31164493ba0720982c3c7495
Joshua Santos <JoshCSan@gmail.com>
2023-06-05 10:23:28 -0700

    - chore: forgot other files

--- packages/api/src/routers/health/healthRouter.ts
+++
@@ -1,7 +0,0 @@
-import { publicProcedure, createTRPCRouter } from '../../trpc';
-
-export const healthRouter = createTRPCRouter({
-  status: publicProcedure.query(() => {
-    return 'ok';
-  }),
-});
