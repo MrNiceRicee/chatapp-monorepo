8614dd41df7fdf775086cfe0deb683cf99a8fafd
Joshua Santos <JoshCSan@gmail.com>
2023-06-07 01:21:19 -0700

    - chore: update turbo & remove logs

--- apps/server/src/routers/chat/listMessage.ts
+++ apps/server/src/routers/chat/listMessage.ts
@@ -2,10 +2,6 @@ import { publicProcedure } from '../../trpc';
 import { type Message } from '../../types/types';
 
 export const listMessage = publicProcedure.query(async ({ ctx }) => {
-  // return ctx.redis.lrange('public-messages', 0, -1) as unknown as Promise<
-  //   Message[]
-  // >;
-  // return ctx.redis.listPublicMessages('public-messages');
   const messages = await ctx.redis.lrange('public-messages', 0, -1);
 
   return messages.map((message) => JSON.parse(message) as Message);
