cdfa2900a5cec583e1c43c43489643e9457e7574
Joshua Santos <JoshCSan@gmail.com>
2023-06-07 19:24:12 -0700

    - style: some more things

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -3,7 +3,7 @@ import { RouterOutput, api } from './api/trpc';
 
 function PostMessage() {
   const addMessage = api.chat.addMessage.useMutation();
-  const messageRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLTextAreaElement>(null);
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -27,7 +27,7 @@ function PostMessage() {
 
   return (
     <form onSubmit={onSubmit} className="flex w-full flex-col">
-      <div className="w-full space-y-4 overflow-hidden rounded-lg border border-gray-300 shadow-sm focus-within:border-sky-500 focus-within:ring-1 focus-within:ring-sky-500">
+      <div className="w-full space-y-4 overflow-hidden rounded-lg border border-gray-300 shadow-sm focus-within:border-teal-500 focus-within:ring-1 focus-within:ring-teal-500">
         <fieldset className="flex items-center space-x-2">
           <label htmlFor="message" className="sr-only">
             Username
@@ -47,14 +47,15 @@ function PostMessage() {
             rows={2}
             name="message"
             className="block w-full resize-none border-0 bg-inherit py-0 placeholder:text-gray-400 focus:outline-none focus:ring-0 sm:text-sm sm:leading-6"
-            placeholder="Write a description..."
+            placeholder="write anything"
+            ref={messageRef}
           />
         </fieldset>
         <div className="flex flex-col items-end border-t border-zinc-500 p-2">
           <button
             type="submit"
             disabled={addMessage.isLoading || addMessage.error !== null}
-            className="rounded-full border border-sky-500 bg-sky-50 px-4 py-2 text-sky-900 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-sky-950 dark:text-sky-50"
+            className="rounded-full border border-teal-500 bg-teal-50 px-4 py-2 text-teal-900 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-teal-950 dark:text-teal-50"
           >
             {addMessage.isLoading ? 'Sending...' : 'Send'}
           </button>
@@ -120,7 +121,7 @@ function WebsocketList({ messages }: { messages: MessageList }) {
         Chat is {model.connected ? 'connected' : 'disconnected'}
       </p>
       <ul
-        className="flex max-h-[50vh] flex-col space-y-2 overflow-y-scroll rounded-md bg-gradient-to-t from-stone-100 to-stone-50 p-4 shadow-inner shadow-stone-300 dark:from-stone-900 dark:to-stone-900/70 dark:shadow-stone-800"
+        className="flex max-h-[50vh] flex-col space-y-2 overflow-y-scroll overscroll-contain rounded-md bg-gradient-to-t from-stone-100 to-stone-50 p-4 shadow-inner shadow-stone-300 dark:from-stone-900 dark:to-stone-900/70 dark:shadow-stone-800"
         ref={scrollRef}
       >
         {model.data.map((item, index) => (
@@ -128,16 +129,16 @@ function WebsocketList({ messages }: { messages: MessageList }) {
             key={`item-${index}`}
             className="rounded-md px-2 text-left text-sm outline outline-1 outline-gray-300"
           >
-            <p className="flex w-full justify-between font-medium">
-              <span className="text-base">{item.username}</span>
-              <span className="ml-auto font-light">
+            <p className="flex w-full justify-between text-xs">
+              <span className="text-base font-medium">{item.username}</span>
+              <span className="ml-auto font-extralight">
                 {new Intl.DateTimeFormat('en-US', {
                   dateStyle: 'medium',
                   timeStyle: 'short',
                 }).format(item.timestamp)}
               </span>
             </p>
-            <p className="font-light">{item.message}</p>
+            <p className="font-extralight whitespace-pre-wrap">{item.message}</p>
           </li>
         ))}
       </ul>
@@ -170,8 +171,7 @@ function App() {
       <header>
         <h1 className="text-center text-4xl">Idk some chat app</h1>
       </header>
-      {/* <section className="mx-auto flex h-full max-w-sm flex-col justify-between space-y-4 px-2 text-center"> */}
-      <section className="mx-auto max-w-sm px-2 space-y-2">
+      <section className="mx-auto h-full max-w-sm space-y-2 px-2">
         <WebsocketTest />
         <PostMessage />
       </section>
