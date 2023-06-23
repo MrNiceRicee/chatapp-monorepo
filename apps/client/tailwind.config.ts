95d8b114bf017542a2bb10c774b30807c6476b4d
Joshua Santos <JoshCSan@gmail.com>
2023-06-22 22:49:12 -0700

    - update: style

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -1,5 +1,6 @@
 // /** @type {import('tailwindcss').Config} */
 import { type Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
 
 const config = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -7,7 +8,25 @@ const config = {
   theme: {
     extend: {},
   },
-  plugins: [require('tailwindcss-safe-area')],
+  plugins: [require('tailwindcss-safe-area'),
+    plugin(function ({ addComponents }) {
+      addComponents({
+        '.grain-overlay': {
+          position: 'relative',
+          '&::before': {
+            content: '""',
+            position: 'absolute',
+            inset: 0,
+            width: '100%',
+            height: '100%',
+            backgroundImage: "url('/grain.png')",
+            backgroundSize: '250px 250px',
+            pointerEvents: 'none',
+          },
+        },
+      });
+    }),
+],
 } satisfies Config;
 
 export default config;
