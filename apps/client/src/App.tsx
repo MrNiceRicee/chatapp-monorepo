910c6e78f0a21ff3251fd7ad88313f29a608e0bd
MrNiceRicee <joshcsan@gmail.com>
2023-06-28 01:27:44 -0700

    - tweaks

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -37,12 +37,17 @@ export default function App() {
   return (
     <main className="relative flex min-h-screen items-center justify-center overflow-hidden py-4">
       <Background />
-      {hidden ? null : (
-        <Glass className="grain-overlay container mx-auto max-w-2xl px-2 pb-6 pt-2 before:opacity-20">
-          <h1>Silly Chat</h1>
-          <Chat />
-        </Glass>
-      )}
+      <Glass
+        className={classNames(
+          'grain-overlay container mx-auto max-w-2xl px-2 pb-6 pt-2 duration-500 before:opacity-20',
+          hidden
+            ? 'animate-out fade-out fill-mode-forwards slide-out-to-bottom-10'
+            : 'animate-in fade-in fill-mode-forwards slide-in-from-bottom-10',
+        )}
+      >
+        <h1>Silly Chat</h1>
+        <Chat />
+      </Glass>
       <button
         className={classNames(
           'fixed bottom-0 right-0 m-2 rounded-lg border border-zinc-50/10 p-2 text-zinc-50/70 shadow-sm shadow-stone-800/40 duration-200',
